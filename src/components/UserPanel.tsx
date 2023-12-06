9af4a4cc5cf25b7eb32be0c188aa9ea88e13b277
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-12-06 17:55:50 +0100

    fix: yarn.lock file

--- src/components/UserPanel.tsx
+++ src/components/UserPanel.tsx
@@ -25,7 +25,7 @@ export default function UserPanel({active}: Props) {
             title: "SETTINGS",
             icon: "tabler:settings-2",
             to: navigate(`${url}/settings`)
-        }
+        },
     ]
 
   return (
