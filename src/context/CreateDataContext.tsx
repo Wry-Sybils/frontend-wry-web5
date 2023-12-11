436928b7e0413217829c4740a1f5d7256e6f90b1
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-12-11 11:14:57 +0100

    feat: Add content to createdatacontext

---
+++ src/context/CreateDataContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+
+interface DataProps {
+    title: string;
+    description: string;
+    privacy: boolean;
+}
+
+interface CreateDataContextProps {
+    children: ReactNode;
+}
+
+interface CreateDataContextValue {
+    data: DataProps | null;
+    setData: (data: DataProps | null) => void;
+}
+
+const CreateDataContext = createContext<CreateDataContextValue | undefined>(undefined);
+
+export const CreateDataProvider: React.FC<CreateDataContextProps> = ({ children }) => {
+    const [data, setData] = useState<DataProps | null>(() => {
+        const storedData = localStorage.getItem('createdData');
+        return storedData ? JSON.parse(storedData) : null;
+    });
+
+    useEffect(() => {
+        if (data) {
+            localStorage.setItem('createdData', JSON.stringify(data));
+        } else {
+            localStorage.removeItem('createdData');
+        }
+    }, [data]);
+
+    return (
+        <CreateDataContext.Provider value={{ data, setData }}>
+            {children}
+        </CreateDataContext.Provider>
+    );
+};
+
+export const useCreateData = () => {
+    const context = useContext(CreateDataContext);
+    if (context === undefined) {
+        throw new Error('useCreateData must be used within a CreateDataProvider');
+    }
+    return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext.tsx'
 import { UserProvider } from './context/UserContext.tsx'
+import { CreateDataProvider } from './context/CreateDataContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
       <UserProvider>
-        <App />
+        <CreateDataProvider>
+          <App />
+        </CreateDataProvider>
       </UserProvider>
     </ThemeProvider>
   </React.StrictMode>,
